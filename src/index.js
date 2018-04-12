1350607b332a3f84ea59cba435cb82822ff7ec95
AnshulPatni <anshulpatni19@gmail.com>
2018-04-12 09:41:56 -0700

    Added Actions & Action Types

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import chat from './reducers'
+
+import * as types from '../constants/ActionTypes'
+
+let nextMessageId = 0
+let nextUserId = 0
+
+export const addMessage = (message, author) => ({
+  type: types.ADD_MESSAGE,
+  id: nextMessageId++,
+  message,
+  author
+})
+
+export const addUser = name => ({
+  type: types.ADD_USER,
+  id: nextUserId++,
+  name
+})
+
+export const messageReceived = (message, author) => ({
+  type: types.MESSAGE_RECEIVED,
+  id: nextMessageId++,
+  message,
+  author
+})
+
+export const populateUsersList = users => ({
+  type: types.USERS_LIST,
+  users
+})
+
+const store = createStore(chat)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
