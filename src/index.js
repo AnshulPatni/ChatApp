0a168038234e1cb7d3c06513c4af4ee87924c105
AnshulPatni <anshulpatni19@gmail.com>
2018-04-13 09:59:15 -0700

    added changes to the Actions & Action Types

--- src/index.js
+++ src/index.js
@@ -8,36 +8,6 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import chat from './reducers'
 
-import * as types from '../constants/ActionTypes'
-
-let nextMessageId = 0
-let nextUserId = 0
-
-export const addMessage = (message, author) => ({
-  type: types.ADD_MESSAGE,
-  id: nextMessageId++,
-  message,
-  author
-})
-
-export const addUser = name => ({
-  type: types.ADD_USER,
-  id: nextUserId++,
-  name
-})
-
-export const messageReceived = (message, author) => ({
-  type: types.MESSAGE_RECEIVED,
-  id: nextMessageId++,
-  message,
-  author
-})
-
-export const populateUsersList = users => ({
-  type: types.USERS_LIST,
-  users
-})
-
 const store = createStore(chat)
 
 ReactDOM.render(
